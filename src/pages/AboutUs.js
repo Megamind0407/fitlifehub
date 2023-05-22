cb1e1d89c0fd4b0df20a4e4a8cb3394561923a1d
poseidonsanket <112609618+poseidonsanket@users.noreply.github.com>
2023-05-22 12:20:56 +0530

    About Us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,27 @@
 import React from "react";
+import aboutus from "../assets/images/aboutus.jpg";
+import { makeStyles } from '@material-ui/core/styles';
 
+import { Box } from '@mui/material';
+
+const useStyles = makeStyles({
+  image: {
+    width: '500px', // Set the desired width
+    height: '400px', // Set the desired height
+  },
+});
 
 export default function AboutUs() {
+  const classes = useStyles();
+
   return (
-    <div>
-      About us
-    </div>
+    <Box display="flex" justifyContent="flex-end">
+    <img
+    className={classes.image}
+    src={aboutus}
+    alt=""
+  />
+  </Box>
       
   );
 }
