93b86c81e6984a5269258c3c8bc06dd62298c902
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2023-05-24 22:49:40 +0530

    about us changes

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,18 +2,21 @@ import React from "react";
 import aboutus from "../assets/images/aboutus.jpg";
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Box } from '@mui/material';
+ import { Box } from '@mui/material';
+
+
 
 const useStyles = makeStyles({
   image: {
     width: '500px', // Set the desired width
     height: '400px', // Set the desired height
+    borderRadius: '8px', // Adjust the value as per your preference
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
   },
 });
 
 export default function AboutUs() {
   const classes = useStyles();
-
   return (
     <Box display="flex" justifyContent="flex-end">
     <img
@@ -21,7 +24,8 @@ export default function AboutUs() {
     src={aboutus}
     alt=""
   />
+   
   </Box>
-      
   );
-}
+} 
+
